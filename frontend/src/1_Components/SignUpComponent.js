d1d8b7c6671cf5047415463dbf4d56e3a46434c9
Zen H. A-K <zen@mail.com>
2022-09-09 20:32:18 +0200

    Desbut signup component

--- frontend/src/1_Components/SignUpComponent.js
+++ frontend/src/1_Components/SignUpComponent.js
@@ -4,7 +4,23 @@ const SignUpComponent = () =>
 {
     return(
         <React.StrictMode>
-            <h1>Page d'Inscription - placeholder</h1>
+            <h1>Inscription</h1>
+            <p>Vous pouvez vous inscrire ici au réseau interne de Groupomania!</p>
+            <form>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name="email" id="email" placeholder="john.doe@mail.com"></input>
+                </div>
+                <div>
+                    <label htmlFor="password">Mot de passe</label>
+                    <input type="password" name="password" id="password"></input>
+                </div>
+                <div>
+                    <label htmlFor="passwordRepeated">Veuillez répéter le mot de passe</label>
+                    <input type="password" name="passwordRepeated" id="passwordRepeated"></input>
+                </div>
+                <button type="submit">S'inscrire</button>
+            </form>
         </React.StrictMode>
     )
 }
